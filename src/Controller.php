fa49e68383196ca7b5175165aaa332e104b5deb2
AmirBehzad Eslami <behzad.eslami@gmail.com>
2015-12-20 00:43:52 +0800

    Added new methods in Controller to access Request, Response, and Container

--- src/Controller.php
+++ src/Controller.php
@@ -18,7 +18,22 @@ abstract class Controller
         $this->args = $args;
     }
 
-    protected function get($service)
+    public function getParam($key, $default = null)
+    {
+        return array_key_exists($key, $this->args) ? $this->args[$key] : $default;
+    }
+
+    public function getRequest()
+    {
+        return $this->req;
+    }
+
+    public function getResponse()
+    {
+        return $this->res;
+    }
+
+    protected function getService($service)
     {
         return $this->container->get($service);
     }
