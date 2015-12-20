09fba3757917cc1b0c2ba62a2100b3a5ea6c9994
AmirBehzad Eslami <behzad.eslami@gmail.com>
2015-12-20 14:59:43 +0800

    Implemented the Web::redirect() with unit-test

--- src/Controller/Web.php
+++ src/Controller/Web.php
@@ -3,11 +3,23 @@
 namespace Abridger\Controller;
 
 use \Abridger\Controller;
+use \Abridger\Hasher;
 
 class Web extends Controller
 {
     public function redirect()
     {
+        $token = $this->getParam('token');
+        $url_id = Hasher::decode($token);
+
+        $query = $this->getService('DB')->prepare('SELECT * from urls WHERE id = :url_id');
+        $query->bindParam(':url_id', $url_id);
+        $query->execute();
+        $result = $query->fetch(\PDO::FETCH_ASSOC);
+
+        return ($result) ?
+            $this->getResponse()->withRedirect($result['url']) :
+            $this->getResponse()->withStatus(404)->write('Not Found');
     }
 
     public function homepage()
--- tests/WebControllerTest.php
+++ tests/WebControllerTest.php
@@ -3,6 +3,10 @@
 namespace Abridger\Tests;
 
 use \Abridger\App as App;
+use \Slim\Http\Environment as Env;
+use \Slim\Http\Request;
+use \Slim\Http\Response;
+use \Slim\Container;
 
 class WebControllerTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,5 +16,41 @@ class WebControllerTest extends \PHPUnit_Framework_TestCase
         $this->assertStringEndsWith('Abridger\Controller', get_parent_class('\Abridger\Controller\Web'));
         $this->assertTrue(method_exists('\Abridger\Controller\Web', 'homepage'));
         $this->assertTrue(method_exists('\Abridger\Controller\Web', 'redirect'));
+    }
+
+    public function getMockedController($token, $result)
+    {
+        $req = Request::createFromEnvironment(Env::mock());
+        $res = new Response();
+        $args = ['token' => $token];
+
+        $db = $this->getMockBuilder('\Abridger\DB')
+            ->disableOriginalConstructor()
+            ->setMethods(['prepare'])
+            ->getMock();
+
+        $query = $this->getMock('\PDOStatement');
+        $query->method('fetch')->willReturn($result);
+        $db->method('prepare')->willReturn($query);
+
+        $cnt = new Container(['DB' => $db]);
+
+        return new \Abridger\Controller\Web($cnt, $req, $res, $args);
+    }
+
+    public function testRedirectNotFound()
+    {
+        $response = $this->getMockedController('XYZ', false)->redirect();
+        $this->assertTrue($response->isNotFound());
+        $this->assertFalse($response->isRedirect());
+    }
+
+    public function testRedirectOk()
+    {
+        $url = 'http://www.mindvally.com';
+        $response = $this->getMockedController('xyz', ['url' => $url])
+            ->redirect();
+        $this->assertTrue($response->isRedirect());
+        $this->assertContains($url, (string) $response);
     }
 }
