19ea5fe7c8151a1e07a90937c7b51acb7dba3aee
AmirBehzad Eslami <behzad.eslami@gmail.com>
2015-12-20 17:00:04 +0800

    Implemented ApiController with tests

--- src/Controller/Api.php
+++ src/Controller/Api.php
@@ -3,11 +3,48 @@
 namespace Abridger\Controller;
 
 use \Abridger\Controller;
+use \Abridger\Hasher;
 
 class Api extends Controller
 {
-    public function abrdige()
+
+    const MAX_URL_LEN = 255;
+
+    public function abridge()
     {
-        return $this->res->getBody()->write('Test');
+        $url = $this->getParam('url');
+
+        if (empty($url)) {
+            return $this->output(false, 'No URL is provided');
+        }
+        if (! filter_var($url, FILTER_VALIDATE_URL)) {
+            return $this->output(false, 'The given URL is malformed');
+        }
+        if (strlen($url) > self::MAX_URL_LEN) {
+            return $this->output(false, 'The given URL exceeds the maximum length');
+        }
+
+        $query = $this->getService('DB')
+            ->prepare('INSERT INTO urls (url) VALUES (:url) ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id)');
+        $query->bindParam(':url', $url);
+        $query->execute();
+
+        $url_id = $this->getService('DB')->lastInsertId();
+
+        if (! $url_id) {
+            return $this->output(false, 'Unable to generate a token for the given URL');
+        }
+
+        $token = Hasher::encode($url_id);
+        return $this->output(true, 'OK', $token);
+    }
+
+    private function output($success, $message, $data = '')
+    {
+        return $this->getResponse()->withJson([
+          'success' => $success,
+          'message' => $message,
+          'data' => $data,
+        ]);
     }
 }
--- tests/ApiControllerTest.php
+++ tests/ApiControllerTest.php
@@ -3,6 +3,10 @@
 namespace Abridger\Tests;
 
 use \Abridger\App as App;
+use \Slim\Http\Environment as Env;
+use \Slim\Http\Request;
+use \Slim\Http\Response;
+use \Slim\Container;
 
 class ApiControllerTest extends \PHPUnit_Framework_TestCase
 {
@@ -10,6 +14,73 @@ class ApiControllerTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertTrue(class_exists('\Abridger\Controller\Api'));
         $this->assertStringEndsWith('Abridger\Controller', get_parent_class('\Abridger\Controller\Api'));
-        $this->assertTrue(method_exists('\Abridger\Controller\Api', 'abrdige'));
+        $this->assertTrue(method_exists('\Abridger\Controller\Api', 'abridge'));
+    }
+
+    public function getMockedController($url, $result)
+    {
+        $req = Request::createFromEnvironment(Env::mock(
+            [
+                'REQUEST_METHOD' => 'POST',
+                'url' => $url,
+            ]
+        ));
+        $req = $req->withParsedBody(['url' => $url]);
+        $res = new Response();
+        $args = ['url' => $url];
+
+        $db = $this->getMockBuilder('\Abridger\DB')
+            ->disableOriginalConstructor()
+            ->setMethods(['prepare', 'lastInsertId'])
+            ->getMock();
+
+        $query = $this->getMock('\PDOStatement');
+        $db->method('prepare')->willReturn($query);
+        $db->method('lastInsertId')->willReturn($result);
+
+        $cnt = new Container(['DB' => $db]);
+
+        return new \Abridger\Controller\Api($cnt, $req, $res, $args);
+    }
+
+    public function testInvalidUrl()
+    {
+        $response = $this->getMockedController('htp//:invalid_url.com', false)->abridge();
+        $output = (string) $response->getBody();
+        $json = json_decode($output);
+        $this->assertNotFalse($json);
+        $this->assertFalse($json->success);
+        $this->assertContains('URL is malformed', $json->message);
+    }
+
+    public function testEmptyUrl()
+    {
+        $response = $this->getMockedController('', false)->abridge();
+        $output = (string) $response->getBody();
+        $json = json_decode($output);
+        $this->assertNotFalse($json);
+        $this->assertFalse($json->success);
+        $this->assertContains('No URL', $json->message);
+    }
+
+    public function testDatabaseProblem()
+    {
+        $response = $this->getMockedController('http://www.valid.com', false)->abridge();
+        $output = (string) $response->getBody();
+        $json = json_decode($output);
+        $this->assertNotFalse($json);
+        $this->assertFalse($json->success);
+        $this->assertContains('Unable to generate', $json->message);
+    }
+
+    public function testSuccessToken()
+    {
+        $response = $this->getMockedController('http://www.valid.com', 1984)->abridge();
+        $output = (string) $response->getBody();
+        $json = json_decode($output);
+        $this->assertNotFalse($json);
+        $this->assertTrue($json->success);
+        $this->assertContains('OK', $json->message);
+        $this->assertEquals('ypjggpvj', $json->data);
     }
 }
